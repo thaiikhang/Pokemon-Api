3480ecdc3a259f156d73092eea6413be475bd473
Khaug <huynhkhang690@gmail.com>
2023-02-16 11:31:53 +0700

    Done: type format for props and output pokemon's name and picture to the screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
+import PokemonCollection from './components/PokemonCollection';
+import { Pokemon } from "./interface";
 
 interface Pokemons {
   name:string;
   url:string;
-}
-
-interface Pokemon {
-  id: number;
-  name: string;
-  sprites: {
-    front_default: string;
-  };
 }
 
 const App:React.FC = () => {
@@ -20,9 +14,10 @@ const App:React.FC = () => {
   useEffect(() => {
     const getPokemon = async() => {
       const res = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=20&offset=20");
+      console.log(res.data)
       res.data.results.forEach(async(pokemon:Pokemons) => {
         const poke = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
-        setPokemons((p) => [...p,poke.data]); //convert from object to array for use Map
+        setPokemons((p) => [...p, poke.data]); //convert from object to array for use Map
       })
     }
     getPokemon();
@@ -31,6 +26,7 @@ const App:React.FC = () => {
   <div className='App'>
     <div className="container">
       <header className="pokemon-header">Pokemon</header>
+      <PokemonCollection pokemons={pokemons}/>
     </div>
   </div>
   );
---
+++ src/components/PokemonCollection.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Pokemon } from '../interface';
+import PokemonList from './PokemonList';
+import "./pokemon.css";
+
+interface Props {
+    pokemons: Pokemon[];
+};
+
+const pokemoncollection:React.FC<Props> = (props) => {
+    const {pokemons} = props;
+  return (
+    <div>
+      <section className="collection-container">
+        {pokemons.map((pokemon) => {
+            return(
+                <div className=''>
+                    <PokemonList
+                    key={pokemon.id}
+                    name={pokemon.name}
+                    id={pokemon.id}
+                    image={pokemon.sprites.front_default}
+                    />
+                </div>
+            )
+        })}
+      </section>
+    </div>
+  )
+}
+
+export default pokemoncollection
