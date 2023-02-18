7d2b6ad8990186bb7c3c0858932f843f83b63490
Khaug <huynhkhang690@gmail.com>
2023-02-18 22:39:32 +0700

    Done: Loadmore button

--- src/App.tsx
+++ src/App.tsx
@@ -11,22 +11,46 @@ interface Pokemons {
 
 const App:React.FC = () => {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  const [nextUrl, setNextUrl] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(true);
   useEffect(() => {
-    const getPokemon = async() => {
-      const res = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=20&offset=20");
-      console.log(res.data)
-      res.data.results.forEach(async(pokemon:Pokemons) => {
-        const poke = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
-        setPokemons((p) => [...p, poke.data]); //convert from object to array for use Map
-      })
-    }
+    const getPokemon = async () => {
+      const res = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon?limit=20&offset=20"
+      );
+      setNextUrl(res.data.next);
+      res.data.results.forEach(async (pokemon: Pokemons) => {
+        const poke = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+        );
+        setPokemons((p) => [...p, poke.data]);
+        setLoading(false);
+      });
+    };
     getPokemon();
-  },[])
+  }, []);
+
+  const nextPage = async () => {
+    setLoading(true);
+    let res = await axios.get(nextUrl);
+    setNextUrl(res.data.next);
+    res.data.results.forEach(async (pokemon: Pokemons) => {
+      const poke = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+      );
+      setPokemons((p) => [...p, poke.data]);
+      setLoading(false);
+    });
+  };
+
   return (
   <div className='App'>
     <div className="container">
       <header className="pokemon-header">Pokemon</header>
       <PokemonCollection pokemons={pokemons}/>
+      <div className="btn">
+        <button onClick={nextPage}> {loading ? "Loading..." : "Load more"} </button>
+      </div>
     </div>
   </div>
   );
